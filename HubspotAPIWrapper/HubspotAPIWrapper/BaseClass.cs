e4e6bc830555a7b0b433b4064656e12607296a19
Tom Atnip <tom.atnip@gmail.com>
2013-07-09 17:21:11 -0400

    Create Project

---
+++ HubspotAPIWrapper/HubspotAPIWrapper/BaseClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubspotAPIWrapper
+{
+    public class BaseClass
+    {
+        private string api_key;
+        private string access_token;
+        private string refresh_token;
+        private string client_id;
+        private Dictionary<string, object> options;
+        public BaseClass(string api_key=null, string access_token=null, string refresh_token=null, string client_id=null)
+        {
+            this.api_key = api_key;
+            this.access_token = access_token;
+            this.refresh_token = refresh_token;
+            this.client_id = client_id;
+
+            if ((this.api_key.Equals(null)) && (this.access_token.Equals(null)))
+            {
+                throw new ArgumentException("Cannot use both api_key and access_token");
+            }
+            if ((this.api_key.Equals(null)) & (this.access_token.Equals(null)) && (this.refresh_token.Equals(null)))
+            {
+                throw new ArgumentException("Missing required credentials");
+            }
+            this.options = new Dictionary<string, object>(); //{"api_base": "api.hubapi.com"};
+            this.options["api_base"] = "api.hubapi.com";
+        }
+
+        protected object call(string subpath, Dictionary<string, object> parameters=null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HubspotAPIWrapper/HubspotAPIWrapper/Prospects.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubspotAPIWrapper
+{
+    class Prospects : BaseClass
+    {
+        private string PROSPECTS_API_VERSION = "v1";
+
+        public string get_path(string method)
+        {
+            return string.Format("prospects/{0}/{1}", PROSPECTS_API_VERSION, method);
+        }
+
+        public object get_prospects(int offset=0, string orgoffset=null, int limit=0)
+        {
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+
+            if (limit > 0)
+            {
+                parameters["count"] = limit;
+            }
+
+            if (offset > 0)
+            {
+                parameters["timeOffset"] = offset;
+                parameters["orgOffset"] = orgoffset;
+            }
+
+            return this.call("timeline", parameters);
+        }
+
+        object get_company(string company_slug)
+        {
+            return this.call(string.Format("timeline/{0}", company_slug));
+        }
+    }
+}
