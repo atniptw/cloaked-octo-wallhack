107e93528224b5ec7adc0c63b80a6592d7a3d9ae
Tom Atnip <tom.atnip@gmail.com>
2013-07-23 18:17:49 -0400

    Remove unused WebClient

--- HubspotAPIWrapper/HubspotAPIWrapper/WindowsWebClient.cs
+++ HubspotAPIWrapper/HubspotAPIWrapper/WindowsWebClient.cs
@@ -5,22 +5,22 @@ namespace HubspotAPIWrapper
 {
     internal class WindowsWebClient : IWebClient
     {
-        private readonly WebClient client;
-
         public string UploadString(string uri, string method = "GET", string contentType = "application/text", string data = "")
         {
-            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(uri);
+            var request = (HttpWebRequest) WebRequest.Create(uri);
             request.Method = method;
+            request.ContentType = contentType;
+
+            if (data.Length > 0)
+            {
+                var writer = new StreamWriter(request.GetRequestStream());
+                writer.Write(data);
+            }
 
             var response = request.GetResponse();
             var responseStream = response.GetResponseStream();
             var streamReader = new StreamReader(responseStream);
             return streamReader.ReadToEnd();
-/*
-            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-            client.Headers.Add("Content-Type", contentType);
-
-            return client.UploadString(uri, method, data);*/
         }
     }
 }
